299c46652447782cfe63b5021c3c0ff646029442
Ektaynot <msinantop@gmail.com>
2024-03-24 11:13:04 +0300

    added term2anki

--- config.el
+++ config.el
@@ -190,6 +190,23 @@
   (insert "</div>\n")
   (insert "#+END_EXPORT\n"))
 
+(defun term2anki (file)
+  (interactive "FExport notes to: ")
+  (let ((regex (rx bol (in "+-") " " (group (1+ nonl)) ": " (group (1+ nonl))))
+        (buf (find-file-noselect file))
+        (output ""))
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward regex nil t)
+        (setq output (concat output (format "%s;%s\n" (match-string 1)
+                                            (match-string 2)))))
+      (with-current-buffer buf
+        (erase-buffer)
+        (insert output)
+        (save-buffer))
+      (kill-buffer buf)
+      (message "Export done."))))
+
 (set-file-template! "\\.org$" :trigger "__orgtemplate.org" :mode 'org-mode)
 
 (setq browse-url-mailto-function 'browse-url-generic)
